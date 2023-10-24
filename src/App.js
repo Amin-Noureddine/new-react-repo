2b23b0ff1a4651fe536ec3af44f45e043a97c6f3
Amin-Noureddine <amin.agrebi2023@hotmail.com>
2023-10-24 08:29:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
-import Landing_Page from './Landing_Page/Landing_Page';
-function App() {
+import SignUp from './Sign_Up/Sign_Up'; // Corrected import path and component name
+import Login from './Login/Login';
+import LandingPage from './Landing_Page/Landing_Page';
 
+function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Navbar/>
-              <Routes>
-              <Route path="/Landing_Page" element={<Landing_Page/>}/>
-              </Routes>
-            
-        </BrowserRouter>
-       
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/landingpage" element={<LandingPage />} />
+
+          {/* Define other routes as needed */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
