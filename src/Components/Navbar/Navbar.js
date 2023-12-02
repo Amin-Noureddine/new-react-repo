cf0f1e0b1aee72a81ba3cf68cbb45c3a60d797b2
Amin-Noureddine <amin.agrebi2023@hotmail.com>
2023-12-02 09:05:55 -0500

    initial commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,48 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import "./Navbar.css";
 
+
+
 const Navbar = () => {
-  const [click, setClick] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
+    const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState("");
+    const[email,setEmail]=useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const handleClick = () => setClick(!click);
 
-  const handleLogout = () => {
-    // Clear user data and log the user out
-    sessionStorage.removeItem("auth-token");
-    sessionStorage.removeItem("name");
-    sessionStorage.removeItem("email");
-    sessionStorage.removeItem("phone");
-    // Remove other user data if needed
-
-    // Update the state to reflect the user's logout status
-    setIsLoggedIn(false);
-    setUsername("");
-  };
-
-  useEffect(() => {
-    // Check if the user is logged in
-    const storedUsername = sessionStorage.getItem("name");
-
-    if (storedUsername) {
-      setIsLoggedIn(true);
-      setUsername(storedUsername);
+    
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        // remove email phone
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        // setUsername("");
+       
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+          }
+        }
+        setEmail('');
+        window.location.reload();
     }
-  }, []);
+    const handleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    }
+    useEffect(() => { 
+      const storedemail = sessionStorage.getItem("email");
 
+      if (storedemail) {
+            setIsLoggedIn(true);
+            setUsername(storedemail);
+          }
+        }, []);
   return (
     <nav>
       <div className="nav__logo">
-        <Link to="/">StayHealthy</Link>
-        <i style={{ color: "#2190FF" }} className="fa fa-user-md"></i>
+        <Link to="/">
+        StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i></Link>
         <span>.</span>
       </div>
       <div className="nav__icon" onClick={handleClick}>
         <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
       </div>
-      <ul className={click ? "nav__links active" : "nav__links"}>
+      <ul className={click ? 'nav__links active' : 'nav__links'}>
         <li className="link">
           <Link to="/">Home</Link>
         </li>
@@ -53,18 +67,16 @@ const Navbar = () => {
           <Link to="/healthblog">Health Blog</Link>
         </li>
         <li className="link">
-          <Link to="/reviews">Reviews</Link>
+         <Link to="/reviews">Reviews</Link>
         </li>
-        {isLoggedIn ? (
+        {isLoggedIn?(
           <>
-            <li className="link">
-              Welcome, {username}
-            </li>
             <li className="link">
               <button className="btn2" onClick={handleLogout}>
                 Logout
               </button>
             </li>
+            
           </>
         ) : (
           <>
